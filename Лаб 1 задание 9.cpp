7ad4dc73309b049b103bcdd367a268d3b0d5f587
DevillTwizz <71434000+DevillTwizz@users.noreply.github.com>
2020-09-17 12:17:15 +0300

    Add files via upload

---
+++ Лаб 1 задание 9.cpp
@@ -0,0 +1,25 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int
+main()
+{
+	int Fa = 0;
+	int Fb = 1;
+	int n = 0;
+
+	while (n < 1000) {
+		
+		n = Fa + Fb;
+		Fb = Fa;
+		Fa = n;
+
+	} 
+
+	printf("%d", n);
+
+	
+	
+	return 0;
+
+}
+
